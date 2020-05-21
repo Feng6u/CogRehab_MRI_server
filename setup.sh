9476531b05a9d171df16b9752fb9def8f1295942
Feng <aagufeng@gmail.com>
2020-05-21 19:12:40 -0400

    The following changes are reflected: 1. change in file structures; 2. change in ID naming (changed to ID_TP, where TP1 is pre-intervention and TP2 is post-intervention; 3. added more comments

--- setup.sh
+++ setup.sh
@@ -1,83 +1,139 @@
 #!/bin/bash
 
-ID=$1
+#This shell script creates all required folders for a specific participant at a specific time point to prepare for FreeSurfer analysis, and also copies over the MRI and behavoiral data files from FENG_DIR.
+#Created by Feng Gu in September 2019. Modified by Feng Gu in May 2020. 
 
-	## verify if $SUBJECTS_DIR is set
-if [ -z "$SUBJECTS_DIR" ]
-	then
-	echo "---------------------------------------------------------------------------------------------------------
-ERROR:
-\$SUBJECTS_DIR is not set. Please type "COGREH" before proceeding.
+
+###################### SETTING UP ###########################
+
+ID=$1   #take the first argument as the variable ID
+TP=$2   #take the second argument as the variable TP
+
+if [ -z ${ID} ]    #if user forgets to input ID: 
+then
+        echo "---------------------------------------------------------------------------------------------------------
+ERROR: Subject ID was not supplied. The subject ID (e.g., 'CR000') should be supplied as the first argument.
+Example: "/bin/bash /group/guimond_CogReh/CogRehab/scripts/setup.sh CR001 1"
 ---------------------------------------------------------------------------------------------------------"
 
 exit 1
 
-	## verify if subject ID was supplied as argument
-elif [ -z ${ID} ] 
-then 
-	echo "---------------------------------------------------------------------------------------------------------
-ERROR: No argument was supplied. The subject ID (e.g., 'CR000') should be supplied as the first argument.
-Example:  './setup.sh CR000'
+
+elif [ -z ${TP} ]  #if user forgets to input TP: 
+then
+        echo "---------------------------------------------------------------------------------------------------------
+ERROR: Time point was not supplied. The time point (e.g., '1' or '2') should be supplied as the second argument.
+Example: "/bin/bash /group/guimond_CogReh/CogRehab/scripts/setup.sh CR001 1"
 ---------------------------------------------------------------------------------------------------------"
 
 exit 2
 
+
+elif [ ${#ID} -ne 5 ]  #if user enters ID incorrectly (length of ID is not equal to 5 characters): 
+
+then 
+	echo  "--------------------------------------------------------------------------------------------------------
+ERROR: Please type the full ID of the particiapnt, including the letters (e.g., CR001) 
+---------------------------------------------------------------------------------------------------------"
+
+exit 3
+
+
+elif [ ${#TP} -ne 1 ]  #if user enters TP incorrectly (length of TP is not equal to 1 characters):
+
+then 
+	echo  "---------------------------------------------------------------------------------------------------------
+ERROR: Please type the number for time point. Please do not include the letters (e.g., 1) 
+---------------------------------------------------------------------------------------------------------"
+
+exit 4
+
+
 fi
 
-MRI_DIRS="T1 RESTING VM/VM_Encoding/Run1 VM/VM_Encoding/Run2 VM/VM_Recog EFN_BACK/Run1 EFN_BACK/Run2 FB/Run1 FB/Run2 NM"
+
+######################### DEFINING VARIABLES ##########################
+
 BEH_DIRS="VM/VM_Encoding VM/VM_Recog EFN_BACK FB"
 COGREH_DIR="$(dirname "$SUBJECTS_DIR")"
 FENG_DIR="/group/guimond_CogReh/feng/CogRehab"
+ONE_RUN="T1 RESTING VM/VM_Recog NM"  #these 4 MRI tasks have 1 run each. Define them as ONE_RUN
+TWO_RUNS="VM/VM_Encoding EFN_BACK FB" #these 3 MRI tasks have 2 runs each. Define them as TWO_RUNS
+RUNS="Run1 Run2" #define RUNS as Run1 and Run2. Will be used to name folders
+DIRS="bold anat resting nm par_file" #these are the folders needed for every ID_TP under "subjects" folder
+TASKS="001 002 003 004 005 006 007" #these are folders needed under "bold" folder, one folder for each fMRI task
 
-for i in $MRI_DIRS
 
+########################### CREATING/POPULATING FOLDERS ###########################
+
+#for all the scans that have one run (defined as ONE_RUN), create a folder for each ID_TP, and then copy data from FENG_DIR into raw_data folder
+for i in $ONE_RUN
 do 
 
-mkdir -p $COGREH_DIR/raw_data/MRI_scan_data/$i/$ID
-
-cp $FENG_DIR/MRI_scan_data/$i/$ID/nii/*.nii $COGREH_DIR/raw_data/MRI_scan_data/$i/$ID
-
-done
-
-for o in $BEH_DIRS
-
-do 
-
-mkdir -p $COGREH_DIR/raw_data/fMRI_behavioral_data/$o/$ID
-
-cp $FENG_DIR/fMRI_behavioral_data/$o/$ID/* $COGREH_DIR/raw_data/fMRI_behavioral_data/$o/$ID
+	mkdir -p $COGREH_DIR/raw_data/MRI_scan_data/$i/${ID}_TP${TP}
+	cp $FENG_DIR/MRI_scan_data/$i/${ID}_TP${TP}/nii/*.nii $COGREH_DIR/raw_data/MRI_scan_data/$i/${ID}_TP${TP}
 
 done
 
 
-TASKS="001 002 003 004 005 006 007"
-
-for i in $TASKS
+#for all the scans that have two runs (defined as TWO_RUNS), create a folder for each ID_TP for each run, and then copy data from FENG_DIR into raw_data folder
+for i in $TWO_RUNS
 do
-mkdir -p $COGREH_DIR/subjects/$ID/bold/$i
+	for o in $RUNS
+	do
+		mkdir -p $COGREH_DIR/raw_data/MRI_scan_data/$i/${ID}_TP${TP}/$o
+		cp $FENG_DIR/MRI_scan_data/$i/${ID}_TP${TP}/$o/nii/*.nii $COGREH_DIR/raw_data/MRI_scan_data/$i/${ID}_TP${TP}/$o
+
+	done
 done
 
-mkdir $COGREH_DIR/subjects/$ID/anat
-mkdir $COGREH_DIR/subjects/$ID/resting
-mkdir $COGREH_DIR/subjects/$ID/nm
-mkdir $COGREH_DIR/subjects/$ID/par_file
 
-echo ${ID} > $COGREH_DIR/subjects/$ID/subjectname
+#create folders for behavorial data for each ID_TP, and then copy data from FENG_DIR into raw_data folder
+for i in $BEH_DIRS
+do 
 
-cp $FENG_DIR/MRI_scan_data/T1/$ID/nii/T1.nii $COGREH_DIR/subjects/$ID/anat
-cp $FENG_DIR/MRI_scan_data/VM/VM_Encoding/Run1/$ID/nii/f.nii $COGREH_DIR/subjects/$ID/bold/001
-cp $FENG_DIR/MRI_scan_data/VM/VM_Encoding/Run2/$ID/nii/f.nii $COGREH_DIR/subjects/$ID/bold/002
-cp $FENG_DIR/MRI_scan_data/VM/VM_Recog/$ID/nii/f.nii $COGREH_DIR/subjects/$ID/bold/003
-cp $FENG_DIR/MRI_scan_data/EFN_BACK/Run1/$ID/nii/f.nii $COGREH_DIR/subjects/$ID/bold/004
-cp $FENG_DIR/MRI_scan_data/EFN_BACK/Run2/$ID/nii/f.nii $COGREH_DIR/subjects/$ID/bold/005
-cp $FENG_DIR/MRI_scan_data/FB/Run1/$ID/nii/f.nii $COGREH_DIR/subjects/$ID/bold/006
-cp $FENG_DIR/MRI_scan_data/FB/Run2/$ID/nii/f.nii $COGREH_DIR/subjects/$ID/bold/007
-cp $FENG_DIR/MRI_scan_data/RESTING/$ID/nii/resting.nii $COGREH_DIR/subjects/$ID/resting/resting.nii
-cp $FENG_DIR/MRI_scan_data/NM/$ID/nii/f.nii $COGREH_DIR/subjects/$ID/nm/f.nii
+	mkdir -p $COGREH_DIR/raw_data/fMRI_behavioral_data/$i/${ID}_TP${TP}
+	cp $FENG_DIR/fMRI_behavioral_data/$i/${ID}_TP${TP}/* $COGREH_DIR/raw_data/fMRI_behavioral_data/$i/${ID}_TP${TP}
+
+done
 
 
-##### TMS-EEG #####
-mkdir $COGREH_DIR/TMS_EEG/$ID
-cp $COGREH_DIR/subjects/$ID/anat/T1.nii $COGREH_DIR/TMS_EEG/$ID/
-cp $COGREH_DIR/scripts/normTarg.sh $COGREH_DIR/TMS_EEG/$ID/
 
+#create folders for each ID_TP under subjects folder, to prepare for FreeSurfer
+for i in $DIRS
+do
+	mkdir -p $COGREH_DIR/subjects/${ID}_TP${TP}/$i
+
+done
+
+
+#create 7 folders (defined in TASKS) under bold, one for each run of each fMRI task
+for i in $TASKS 
+do 
+
+	mkdir $COGREH_DIR/subjects/${ID}_TP${TP}/bold/$i
+done
+
+
+#populate folders under subjects folder (copy from FENG_DIR)
+cp $FENG_DIR/MRI_scan_data/T1/${ID}_TP${TP}/nii/T1.nii $COGREH_DIR/subjects/${ID}_TP${TP}/anat
+cp $FENG_DIR/MRI_scan_data/VM/VM_Encoding/${ID}_TP${TP}/Run1/nii/f.nii $COGREH_DIR/subjects/${ID}_TP${TP}/bold/001
+cp $FENG_DIR/MRI_scan_data/VM/VM_Encoding/${ID}_TP${TP}/Run2/nii/f.nii $COGREH_DIR/subjects/${ID}_TP${TP}/bold/002
+cp $FENG_DIR/MRI_scan_data/VM/VM_Recog/${ID}_TP${TP}/nii/f.nii $COGREH_DIR/subjects/${ID}_TP${TP}/bold/003
+cp $FENG_DIR/MRI_scan_data/EFN_BACK/${ID}_TP${TP}/Run1/nii/f.nii $COGREH_DIR/subjects/${ID}_TP${TP}/bold/004
+cp $FENG_DIR/MRI_scan_data/EFN_BACK/${ID}_TP${TP}/Run2/nii/f.nii $COGREH_DIR/subjects/${ID}_TP${TP}/bold/005
+cp $FENG_DIR/MRI_scan_data/FB/${ID}_TP${TP}/Run1/nii/f.nii $COGREH_DIR/subjects/${ID}_TP${TP}/bold/006
+cp $FENG_DIR/MRI_scan_data/FB/${ID}_TP${TP}/Run2/nii/f.nii $COGREH_DIR/subjects/${ID}_TP${TP}/bold/007
+cp $FENG_DIR/MRI_scan_data/RESTING/${ID}_TP${TP}/nii/resting.nii $COGREH_DIR/subjects/${ID}_TP${TP}/resting/resting.nii
+cp $FENG_DIR/MRI_scan_data/NM/${ID}_TP${TP}/nii/nm.nii $COGREH_DIR/subjects/${ID}_TP${TP}/nm/nm.nii
+
+
+#create a file called subjectname that contains ID_TP information for FreeSurfer
+echo ${ID}_TP${TP} > $COGREH_DIR/subjects/${ID}_TP${TP}/subjectname
+
+
+
+####################### TMS-EEG ############################
+mkdir $COGREH_DIR/TMS_EEG/${ID}_TP${TP}  #create a directory for every ID_TP in TMS_EEG folder
+cp $COGREH_DIR/subjects/${ID}_TP${TP}/anat/T1.nii $COGREH_DIR/TMS_EEG/${ID}_TP${TP}/  #copy T1 to the folder
+cp $COGREH_DIR/scripts/normTarg.sh $COGREH_DIR/TMS_EEG/${ID}_TP${TP}/  #copy the script that creates TMS-EEG coordinates to the folder
